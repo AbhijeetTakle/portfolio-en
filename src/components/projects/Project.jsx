1352fb9d469bfafdf02cb9ffeb2254f3a6d2218d
Abhijeet Takle <abhijeettakle35@gmail.com>
2022-08-19 16:44:47 +0530

    changes

--- src/components/projects/Project.jsx
+++ src/components/projects/Project.jsx
@@ -1,22 +1,28 @@
-import React from 'react'
-import './project.css'
-import { BsFillEyeFill } from 'react-icons/bs'
-import { FaGithub } from 'react-icons/fa'
-
+import React from "react";
+import "./project.css";
+import { BsFillEyeFill } from "react-icons/bs";
+import { FaGithub } from "react-icons/fa";
 
 const Project = (props) => {
   return (
-    <div className='project'>
-        <img src={props.src} alt='img'/>
-        <div className='description'>
-            <p>Lorem ipsum dolor sit amet consectetu dolor sit amet consectetudolor sit amet consectetu.</p>
-            <div className='project-links'>
-                <a href={props.gitlink}><FaGithub /></a>
-                <a href={props.demolink}><BsFillEyeFill /></a>
-            </div>    
+    <div className="project">
+      <img src={props.src} alt="img" />
+      <div className="description">
+        <p>
+          Lorem ipsum dolor sit amet consectetu dolor sit amet consectetudolor
+          sit amet consectetu.
+        </p>
+        <div className="project-links">
+          <a href={props.gitlink} target="_blank">
+            <FaGithub />
+          </a>
+          <a href={props.demolink} target="_blank">
+            <BsFillEyeFill />
+          </a>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Project
+export default Project;
